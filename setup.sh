35e0b5118d99e314135f1134a01ee6f02c622f10
jvschoen <jvshoen@live.com>
2020-12-20 02:37:03 -0500

    renamed docker-compose with template extension. made a simple setup script to name the service in the docker-compose file

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
-#repo_name=basename -s .git `git config --get remote.origin.url`
+# Get the name of the repo
+repo_name=$(basename -s .git `git config --get remote.origin.url`)
 
-#sed -i Dockerfile INSERT_NAME_HERE repo_name
+# Making the service name in the docker-compose match the github repo name
+echo "Editing Dockerfile with repo name ($repo_name) as service name"
+sed "s/INSERT_REPO_NAME/${repo_name}/g" docker-compose.yml.tmpl > docker-compose.yml
+
+docker-compose up
